83dce2c6f2d0489df9832082311e4571b5f44567
ayush9817 <96259709+ayush9817@users.noreply.github.com>
2023-12-28 16:39:05 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+
 
 const App = () => {
   return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
     <div className='bg-black'>App</div>
+    
+    </BrowserRouter>
+
+    
   )
 }
 
