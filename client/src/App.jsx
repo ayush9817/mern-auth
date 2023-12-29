06ca5376421e79193340ce467af8ee0c64b437e1
ayush9817 <96259709+ayush9817@users.noreply.github.com>
2023-12-29 12:44:56 +0530

    creat server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
@@ -17,7 +19,7 @@ const App = () => {
       <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/profile' element={<Profile/>}/>
     </Routes>
-    <div className='bg-black'>App</div>
+    
     
     </BrowserRouter>
 
