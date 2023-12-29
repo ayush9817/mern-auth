a20d606e42863694f651db8e0bdccf876c98cb7d
ayush9817 <96259709+ayush9817@users.noreply.github.com>
2023-12-29 13:55:50 +0530

    user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongodb");
+}).catch((err)=>{
+    console.log(err);
+})
+
 app.listen(3000,()=>{
     console.log("server listening on port 3000")
-})
+})    
